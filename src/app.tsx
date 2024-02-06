2f3dbb13aad9a6e1c9f02a53598f152eae3e395c
Luiz D O Santos <lzdavi1325@gmail.com>
2024-02-06 11:46:09 -0300

    feat: add NewCard component

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,5 @@
 import logo from "./assets/logo-nlw-expert.svg";
+import { NewCard } from "./components/new-card";
 import { NoteCard } from "./components/note-card";
 
 export function App() {
@@ -16,16 +17,7 @@ export function App() {
       <div className="h-px bg-slate-700" />
 
       <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
-        <div className="bg-slate-700 p-5 rounded-md">
-          <span className="text-sm font-medium text-slate-200">
-            Adicionar nota
-          </span>
-          <p className="text-sm text-slate-400 mt-3 leading-6">
-            Grave uma nota em áudio que será convertida para texto
-            automaticamente.
-          </p>
-        </div>
-
+        <NewCard />
         <NoteCard />
       </div>
     </div>
